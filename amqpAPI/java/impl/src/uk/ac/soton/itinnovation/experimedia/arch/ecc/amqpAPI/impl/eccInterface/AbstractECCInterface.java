051b001450af52eb10cbedd212277fe95bce6e66
Stephen C. Phillips <scp@it-innovation.soton.ac.uk>
2012-08-07 13:13:06 +0000

    Correcting copyright headers
    git-svn-id: https://svnserver.it-innovation.soton.ac.uk/svn/experimedia/code/ecc/src@101 c5b77668-de2e-634a-a4ea-9167b3f0b12f

--- amqpAPI/java/impl/src/uk/ac/soton/itinnovation/experimedia/arch/ecc/amqpAPI/impl/eccInterface/AbstractECCInterface.java
+++ amqpAPI/java/impl/src/uk/ac/soton/itinnovation/experimedia/arch/ecc/amqpAPI/impl/eccInterface/AbstractECCInterface.java
@@ -17,7 +17,7 @@
 // PURPOSE, except where stated in the Licence Agreement supplied with
 // the software.
 //
-//      Created By :            sgc
+//      Created By :            Simon Crowle
 //      Created Date :          08-Jul-2012
 //      Created for Project :   EXPERIMEDIA
 //
