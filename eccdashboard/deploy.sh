34fba7c2d243795427705873d5b6047a265580b9
Maxim Bashevoy <mvb@it-innovation.soton.ac.uk>
2012-11-15 11:15:08 +0000

    Fixes eccdashboard charm deploy script and paths to config files

--- eccdashboard/deploy.sh
+++ eccdashboard/deploy.sh
@@ -5,12 +5,15 @@ while :; do
     echo Waiting for old service to finish...
     juju destroy-service tomcat
     juju destroy-service eccdashboard
+	juju destroy-service postgresql
+	juju destroy-service rabbitmq-server
   else
     break
   fi
 done
-# juju deploy --repository=/home/tal/experimedia/charms local:postgresql postgresql
-# juju deploy rabbitmq-server
+
+juju deploy postgresql
+juju deploy rabbitmq-server
 
 set -eu
 rm -rf charms/precise/eccdashboard/deploy
@@ -18,10 +21,12 @@ mkdir -p charms/precise/eccdashboard/deploy/eccdashboard
 (cd charms/precise/eccdashboard/deploy/eccdashboard && unzip ../../../../../target/prototype.war)
 cp ../edm/resources/edm-metrics-postgres.sql charms/precise/eccdashboard/
 
-juju deploy --constraints='mem=2G' cs:~robert-ayres/precise/tomcat
+juju deploy cs:~robert-ayres/precise/tomcat
 juju deploy --repository=./charms local:eccdashboard eccdashboard
 juju add-relation eccdashboard tomcat
 juju add-relation eccdashboard:edm postgresql:db
 juju add-relation eccdashboard:rabbit rabbitmq-server
 
 juju expose tomcat
+
+juju debug-log
