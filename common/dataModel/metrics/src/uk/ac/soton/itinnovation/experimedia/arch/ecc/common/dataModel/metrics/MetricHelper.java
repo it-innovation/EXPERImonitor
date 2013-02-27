2cd2e23af0efe6426eae681c94f3cbfbec5a81ea
Simon Crowle <sgc@abbesse.it-innovation.soton.ac.uk>
2013-02-27 11:42:51 +0000

    Added metric generation examples to samples

--- common/dataModel/metrics/src/uk/ac/soton/itinnovation/experimedia/arch/ecc/common/dataModel/metrics/MetricHelper.java
+++ common/dataModel/metrics/src/uk/ac/soton/itinnovation/experimedia/arch/ecc/common/dataModel/metrics/MetricHelper.java
@@ -70,20 +70,47 @@ public class MetricHelper
       
       if ( attr != null && mgenSet != null )
       {
-        UUID targetAttID = attr.getUUID();
-        Set<MeasurementSet> allSets = getAllMeasurementSets( mgenSet );
+        UUID targetAttID              = attr.getUUID();
+        Set<MeasurementSet> allSets   = getAllMeasurementSets( mgenSet );
         Iterator<MeasurementSet> msIt = allSets.iterator();
         
         while ( msIt.hasNext() )
         {
           MeasurementSet ms = msIt.next();
+          UUID linkedAttrID = ms.getAttributeUUID();
           
-          if ( ms.getAttributeUUID().equals(targetAttID) )
+          if ( linkedAttrID != null && linkedAttrID.equals(targetAttID) )
             mSets.put( ms.getUUID(), ms );
         }
       }
       
       return mSets;
+    }
+    
+    public static MeasurementSet getMeasurementSetForAttribute( Attribute attr,
+                                                                MetricGenerator mgen )
+    {
+      MeasurementSet msTarget = null;
+      
+      if ( attr != null && mgen != null )
+      {
+        Map<UUID, MeasurementSet> allSets = getAllMeasurementSets( mgen );
+        Iterator<MeasurementSet> msIt = allSets.values().iterator();
+        
+        while ( msIt.hasNext() )
+        {
+          MeasurementSet ms = msIt.next();
+          UUID linkedAttrID = ms.getAttributeUUID();
+          
+          if ( linkedAttrID != null && linkedAttrID.equals( attr.getUUID()) )
+          {
+            msTarget = ms;
+            break;
+          }
+        }
+      }
+      
+      return msTarget;
     }
     
     public static Map<UUID, Attribute> getAllAttributes( Collection<MetricGenerator> mgenSet )
@@ -116,15 +143,29 @@ public class MetricHelper
         while ( mgenIt.hasNext() )
         {
           MetricGenerator mg = mgenIt.next();
-          Set<Entity> mgEnts = mg.getEntities();
-          if ( mgEnts != null )
+          
+          Map<UUID, Entity> mgEnts = getAllEntities( mg );
+          entities.putAll( mgEnts );
+        }
+      }
+      
+      return entities;
+    }
+    
+    public static Map<UUID, Entity> getAllEntities( MetricGenerator mgen )
+    {
+      HashMap<UUID, Entity> entities = new HashMap<UUID, Entity>();
+      
+      if ( mgen != null )
+      {
+        Set<Entity> mgEnts = mgen.getEntities();
+        if ( mgEnts != null )
+        {
+          Iterator<Entity> mgEntIt = mgEnts.iterator();
+          while ( mgEntIt.hasNext() )
           {
-            Iterator<Entity> mgEntIt = mgEnts.iterator();
-            while ( mgEntIt.hasNext() )
-            {
-              Entity entity = mgEntIt.next();
-              entities.put( entity.getUUID(), entity );
-            }
+            Entity entity = mgEntIt.next();
+            entities.put( entity.getUUID(), entity );
           }
         }
       }
@@ -154,6 +195,29 @@ public class MetricHelper
         return mSets;        
     }
     
+    public static Map<UUID, MeasurementSet> getAllMeasurementSets( MetricGenerator mgen )
+    {
+      HashMap<UUID, MeasurementSet> mSets = new HashMap<UUID, MeasurementSet>();
+      
+      if ( mgen != null )
+      {
+        Iterator<MetricGroup> groupIt = mgen.getMetricGroups().iterator();
+        while ( groupIt.hasNext() )
+        {
+          Iterator<MeasurementSet> msIt = groupIt.next().getMeasurementSets().iterator();
+          while ( msIt.hasNext() )
+          {
+            MeasurementSet ms = msIt.next();
+            UUID msID = ms.getUUID();
+            
+            if ( msID != null ) mSets.put( ms.getUUID(), ms );
+          }
+        }
+      }
+      
+      return mSets;
+    }
+    
     public static MeasurementSet getMeasurementSet( Collection<MetricGenerator> mgenSet,
                                                     UUID measurementSetID )
     {
@@ -171,7 +235,9 @@ public class MetricHelper
                     while ( msIt.hasNext() )
                     { 
                         MeasurementSet ms = msIt.next();
-                        if ( ms.getUUID().equals( measurementSetID) )
+                        UUID msID = ms.getUUID();
+                        
+                        if ( msID != null &&  msID.equals( measurementSetID) )
                         {
                             targetSet = ms;
                             break;
@@ -194,10 +260,196 @@ public class MetricHelper
         while ( mIt.hasNext() )
         {
           Measurement m = mIt.next();
-          sortedM.put( m.getTimeStamp(), m );
+          Date stamp = m.getTimeStamp();
+          
+          if ( stamp != null ) sortedM.put( stamp, m );
         }
       }
       
       return sortedM;
     }
+    
+    public static Attribute createAttribute( String name, String desc, Entity entity )
+    {
+      Attribute attribute = new Attribute();
+      attribute.setName( name );
+      attribute.setDescription( desc );
+      
+      if ( entity != null )
+      {
+        attribute.setEntityUUID( entity.getUUID() );
+        entity.addAttribute( attribute );
+      }
+      
+      return attribute;
+    }
+    
+    public static Attribute getAttributeByName( String name, Entity entity )
+    {
+      Attribute targAttr = null;
+      
+      if ( name != null && entity != null )
+      {
+        Set<Attribute> attrs = entity.getAttributes();
+        if ( attrs != null )
+        {
+          Iterator<Attribute> attIt = attrs.iterator();
+          while ( attIt.hasNext() )
+          {
+            Attribute attr = attIt.next();
+            String attName = attr.getName();
+            
+            if ( attName != null && attName.equals(name) )
+            {
+              targAttr = attr;
+              break;
+            }
+          }
+        }
+      }
+      
+      return targAttr;      
+    }
+    
+    public static MetricGroup createMetricGroup( String name, String desc, MetricGenerator mGen )
+    {
+      MetricGroup mGroup = new MetricGroup();
+      mGroup.setName( name );
+      mGroup.setDescription( desc );
+      
+      if ( mGen != null ) mGen.addMetricGroup( mGroup );
+      
+      return mGroup;
+    }
+    
+    public static MeasurementSet createMeasurementSet( Attribute attr,
+                                                       MetricType type,
+                                                       Unit unit,
+                                                       MetricGroup group )
+    {
+      MeasurementSet mSet = null;
+      
+      if ( attr != null && type != null && unit != null && group != null )
+      {
+        mSet = new MeasurementSet();
+        Metric metric = new Metric();
+        metric.setMetricType( type );
+        metric.setUnit( unit );
+        mSet.setMetric( metric );
+        
+        mSet.setAttributeUUID( attr.getUUID() );
+        mSet.setMetricGroupUUID( attr.getEntityUUID() );
+        
+        group.addMeasurementSets( mSet );
+      }
+      
+      return mSet;
+    }
+    
+    public static MetricGenerator getMetricGeneratorByName( String name, 
+                                                            Set<MetricGenerator> mGens )
+    {
+      MetricGenerator targetGen = null;
+      
+      if ( mGens != null )
+      {
+        Iterator<MetricGenerator> genIt = mGens.iterator();
+        while ( genIt.hasNext() )
+        {
+          MetricGenerator mgen = genIt.next();
+          String genName = mgen.getName();
+          
+          if ( genName != null && genName.equals(name) )
+          {
+            targetGen = mgen;
+            break;
+          }
+        }
+      }
+      
+      return targetGen;
+    }
+    
+    public static MetricGroup getMetricGroupByName( String groupName,
+                                                    Set<MetricGroup> mGroups )
+    {
+      MetricGroup targetGroup = null;
+      
+      if ( groupName != null || mGroups != null )
+      {
+        Iterator<MetricGroup> groupIt = mGroups.iterator();
+        while ( groupIt.hasNext() )
+        {
+          MetricGroup group = groupIt.next();
+          String gName      = group.getName();
+          
+          if ( gName != null && gName.equals(groupName) )
+          {
+            targetGroup = group;
+            break;
+          }
+        }
+      }
+      
+      return targetGroup;
+    } 
+    
+    public static String describeGenerator( MetricGenerator mgen )
+    {
+      String desc = "Could not describe generator (null)";
+
+      if ( mgen != null )
+      {
+        desc = mgen.getName() + "\n";
+        desc += "Description: " + mgen.getDescription() + "\n";
+
+        Collection<Entity> mGenEntities = MetricHelper.getAllEntities(mgen).values();
+        if ( !mGenEntities.isEmpty() )
+        {
+          desc += "Generator has " + mGenEntities.size() + " entities" + "\n";
+
+          Iterator<Entity> entIt = mGenEntities.iterator();
+          while ( entIt.hasNext() )
+          {
+            Entity entity = entIt.next();
+            desc += "  Entity: " + entity.getName() + "\n";
+            desc += "  Description: " + entity.getDescription() + "\n";
+
+            Set<Attribute> attributes = entity.getAttributes();
+            if ( !attributes.isEmpty() )
+            {
+              desc += "  Number of attributes: " + attributes.size() + "\n";
+              Iterator<Attribute> attIt = attributes.iterator();
+              while ( attIt.hasNext() )
+              {
+                Attribute att = attIt.next();
+                desc += "    Attribute: " + att.getName() + "\n";
+                desc += "    Description: " + att.getDescription() + "\n";
+                
+                MeasurementSet ms = getMeasurementSetForAttribute( att, mgen );
+                if ( ms != null )
+                {
+                  desc += "      Measurement set ID: " + ms.getUUID() + "\n";
+                  
+                  Metric metric = ms.getMetric();
+                  if ( metric != null )
+                  {
+                    desc += "      Metric unit: " + metric.getUnit().toString() + "\n";
+                    desc += "      Metric type: " + metric.getMetricType().toString() + "\n";
+                  }
+                  else desc += "      Measurement set has no metric.";
+                }
+                else desc += "    Attribute has no measurement set associated with it.\n";
+              }
+            }
+            else desc += "  Entity has no attributes.\n";
+          }
+        }
+        else desc += "Generator has no entities associated with it.\n";
+        
+        desc += "\n";
+      }
+      
+      return desc;
+    } 
 }
---
+++ samples/metricGeneratorExamples/src/uk/ac/soton/itinnovation/experimedia/arch/ecc/samples/metGenExamples/ExampleMetrics.java
@@ -0,0 +1,212 @@
+/////////////////////////////////////////////////////////////////////////
+//
+// © University of Southampton IT Innovation Centre, 2012
+//
+// Copyright in this software belongs to University of Southampton
+// IT Innovation Centre of Gamma House, Enterprise Road, 
+// Chilworth Science Park, Southampton, SO16 7NS, UK.
+//
+// This software may not be used, sold, licensed, transferred, copied
+// or reproduced in whole or in part in any manner or form or in or
+// on any media by any person other than in accordance with the terms
+// of the Licence Agreement supplied with the software, or otherwise
+// without the prior written consent of the copyright owners.
+//
+// This software is distributed WITHOUT ANY WARRANTY, without even the
+// implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+// PURPOSE, except where stated in the Licence Agreement supplied with
+// the software.
+//
+//      Created By :            Simon Crowle
+//      Created Date :          26-Feb-2013
+//      Created for Project :   EXPERIMEDIA
+//
+/////////////////////////////////////////////////////////////////////////
+
+package uk.ac.soton.itinnovation.experimedia.arch.ecc.samples.metGenExamples;
+
+import uk.ac.soton.itinnovation.experimedia.arch.ecc.common.dataModel.metrics.*;
+
+import java.util.*;
+
+
+
+
+public class ExampleMetrics
+{
+  private Entity                   applicationEntity;
+  private Entity                   userEntity;
+  private HashSet<MetricGenerator> metricGenerators;
+  
+  
+  public ExampleMetrics()
+  {
+  }
+  
+  public void runDemo()
+  {
+    createSystemEntity();
+    
+    createUserEntity();
+    
+    createMetricStructures();
+    
+    createQoSMeasurementSets();
+    
+    createQoEMeasurementSets();
+    
+    MetricGenerator mg = MetricHelper.getMetricGeneratorByName( "Application metric generator",
+                                                                metricGenerators );
+    System.out.println( MetricHelper.describeGenerator(mg) );
+    
+    mg = MetricHelper.getMetricGeneratorByName( "User metric generator",
+                                                 metricGenerators );
+    
+    System.out.println( MetricHelper.describeGenerator(mg) );
+    
+    demoPopulateQoSData();
+    
+    demoPopulateQoEData();
+  }
+  
+  // Private methods -----------------------------------------------------------
+  private void createSystemEntity()
+  {
+    applicationEntity = new Entity();
+    applicationEntity.setName( "Interactive application" );
+    applicationEntity.setDescription( "This entity represents an interactive application a user could use" );
+    
+    MetricHelper.createAttribute( "Application execution time",
+                                  "This attribute represents the total time the application has been running", 
+                                  applicationEntity );
+    
+    MetricHelper.createAttribute( "Application device temperature",
+                                  "This attribute represents the temperature of the device running the application", 
+                                  applicationEntity ); 
+  }
+  
+  private void createUserEntity()
+  {
+    userEntity = new Entity();
+    userEntity.setName( "User" );
+    userEntity.setDescription( "This entity represents the user of an application" );
+    
+    MetricHelper.createAttribute( "Top five user actions",
+                                  "This attribute represents the five most common user actions with the application", 
+                                  userEntity );
+    
+    MetricHelper.createAttribute( "User perceived application speed",
+                                  "This attribute represents a scaled response perceived speed of the application (1=slow.. 5=fast)", 
+                                  userEntity );
+  }
+  
+  private void createMetricStructures()
+  {
+    metricGenerators = new HashSet<MetricGenerator>();
+    
+    MetricGenerator applicationMetricGen = new MetricGenerator();
+    applicationMetricGen.setName( "Application metric generator" );
+    applicationMetricGen.setDescription( "Represents the logical instrumentation for the application" );
+    applicationMetricGen.addEntity( applicationEntity );
+    
+    MetricHelper.createMetricGroup( "Basic QoS",
+                                    "Container for basic QoS metrics", 
+                                    applicationMetricGen );
+    
+    MetricGenerator userMetricGen = new MetricGenerator();
+    userMetricGen.setName( "User metric generator" );
+    userMetricGen.setDescription( "Represents the logical instrumentation for the user" );
+    userMetricGen.addEntity( userEntity );
+    
+    MetricHelper.createMetricGroup( "Basic QoE",
+                                    "Container for basic QoE metrics", 
+                                    userMetricGen );
+    
+    metricGenerators.add( applicationMetricGen );
+    metricGenerators.add( userMetricGen );
+  }
+  
+  private void createQoSMeasurementSets()
+  {
+    MetricGenerator mGen = MetricHelper.getMetricGeneratorByName( "Application metric generator", metricGenerators );
+    MetricGroup group    = MetricHelper.getMetricGroupByName( "Basic QoS", mGen.getMetricGroups() );
+    
+    Attribute attr = MetricHelper.getAttributeByName( "Application execution time", applicationEntity );
+    MetricHelper.createMeasurementSet( attr, 
+                                       MetricType.RATIO,
+                                       new Unit( "seconds"), 
+                                       group );
+    
+    attr = MetricHelper.getAttributeByName( "Application device temperature", applicationEntity );
+    MetricHelper.createMeasurementSet( attr, 
+                                       MetricType.INTERVAL,
+                                       new Unit( "Celcius"), 
+                                       group );
+  }
+  
+  private void createQoEMeasurementSets()
+  {
+    MetricGenerator mGen = MetricHelper.getMetricGeneratorByName( "User metric generator", metricGenerators );
+    MetricGroup group    = MetricHelper.getMetricGroupByName( "Basic QoE", mGen.getMetricGroups()  );
+    
+    Attribute attr = MetricHelper.getAttributeByName( "Top five user actions", userEntity );
+    MetricHelper.createMeasurementSet( attr, 
+                                       MetricType.NOMINAL,
+                                       new Unit( "UI Action dictionary 1"), 
+                                       group );
+    
+    attr = MetricHelper.getAttributeByName( "User perceived application speed", userEntity );
+    MetricHelper.createMeasurementSet( attr, 
+                                       MetricType.ORDINAL,
+                                       new Unit( "Perceived speed"), 
+                                       group );
+  }
+  
+  private void demoPopulateQoSData()
+  {
+    // Create a measurement for execution time
+    MetricGenerator mGen = MetricHelper.getMetricGeneratorByName( "Application metric generator", metricGenerators );
+    Attribute       attr = MetricHelper.getAttributeByName( "Application execution time", applicationEntity );
+    MeasurementSet  ms   = MetricHelper.getMeasurementSetForAttribute( attr, mGen );
+    
+    // Always create new instances of measurement sets to send to the ECC (not implemented here)
+    MeasurementSet newMS = new MeasurementSet( ms, false );
+    Measurement        m = new Measurement( "100" );
+    newMS.addMeasurement( m );
+    System.out.println( "Execution time measurement: " + m.getValue() + " (" +
+                        newMS.getMetric().getUnit().toString() +")" );
+    
+    // Device temperature
+    attr  = MetricHelper.getAttributeByName( "Application device temperature", applicationEntity );
+    ms    = MetricHelper.getMeasurementSetForAttribute( attr, mGen );
+    newMS = new MeasurementSet( ms, false );
+    m     = new Measurement( "28" );
+    newMS.addMeasurement( m );
+    System.out.println( "Application device temperature measurement: " + m.getValue() + " (" +
+                        newMS.getMetric().getUnit().toString() +")" );
+  }
+  
+  private void demoPopulateQoEData()
+  {
+    // Create a sample for top five user actions
+    MetricGenerator mGen = MetricHelper.getMetricGeneratorByName( "User metric generator", metricGenerators );
+    Attribute       attr = MetricHelper.getAttributeByName( "Top five user actions", userEntity );
+    MeasurementSet  ms   = MetricHelper.getMeasurementSetForAttribute( attr, mGen );
+    
+    // Always create new instances of measurement sets to send to the ECC (not implemented here)
+    MeasurementSet newMS = new MeasurementSet( ms, false );
+    Measurement        m = new Measurement( "undo, select, cut, paste, save" );
+    newMS.addMeasurement( m );
+    System.out.println( "Top five actions: " + m.getValue() + " (" +
+                        newMS.getMetric().getUnit().toString() +")" );
+    
+    // Device temperature
+    attr  = MetricHelper.getAttributeByName( "User perceived application speed", userEntity );
+    ms    = MetricHelper.getMeasurementSetForAttribute( attr, mGen );
+    newMS = new MeasurementSet( ms, false );
+    m     = new Measurement( "5" );
+    newMS.addMeasurement( m );
+    System.out.println( "User scaled rating: " + m.getValue() + " (" +
+                        newMS.getMetric().getUnit().toString() +")" );
+  } 
+}
