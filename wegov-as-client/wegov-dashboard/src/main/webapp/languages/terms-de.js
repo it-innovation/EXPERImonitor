264b96062936634815edee4758cd59ff57c991bb
Simon Crowle <sgc@abbesse.it-innovation.soton.ac.uk>
2013-02-28 15:18:02 +0000

    Removed older projects from V1.1-SNAPSHOT:
    eccdashboard (Review prototype)
    wegov-as-client
    wegov-metricgenerators

--- wegov-as-client/wegov-dashboard/src/main/webapp/languages/terms-de.js
+++
@@ -1,9 +0,0 @@
-jquery_lang_js.prototype.lang.de = {
-	'Personal and Business Use' : 'Private und geschäftliche Nutzung',
-	'Additional Terms' : 'Zusätzliche Bedingungen',
-	'WeGov Consortium' : 'WeGov Konsortium',
-	'About' : 'Über uns',
-	'Terms of Use' : 'Nutzungsbedingungen',
-	'Privacy' : 'Datenschutz',
-	'Contact' : 'Kontakt',
-};
