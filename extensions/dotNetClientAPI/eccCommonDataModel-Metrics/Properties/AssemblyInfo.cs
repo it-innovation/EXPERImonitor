2f37655d7b5d7fd07b0df879a430f3f7ad3ddc63
Simon Crowle <sgc@abbesse.it-innovation.soton.ac.uk>
2013-04-15 16:57:50 +0100

    Updated version to 1.2 for C# API
    No snapshot semantics can be directly signed within VS project info.

--- extensions/dotNetClientAPI/eccCommonDataModel-Metrics/Properties/AssemblyInfo.cs
+++ extensions/dotNetClientAPI/eccCommonDataModel-Metrics/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("eccCommonDataModel-Metrics")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("ECC metrics data model")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("University of Southampton IT Innovation Centre")]
 [assembly: AssemblyProduct("eccCommonDataModel-Metrics")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("© Copyright University of Southampton IT Innovation Centre 2012-2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
