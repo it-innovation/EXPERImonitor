d3e7eeb4afff70f494daf96c65b922411244ab4f
Stephen C Phillips <steve@scphillips.com>
2014-01-06 10:53:54 +0000

    Adds comments to vagrantfile and also installs 3rd party libs.

---
+++ thirdPartyLibs/installLibraries.sh
@@ -0,0 +1,5 @@
+echo "Installing OWLIM Lite JAR in local Maven repository"
+
+mvn install:install-file -Dfile=./owlim-lite-5.3.jar -DgroupId=com.ontotext -DartifactId=owlim-lite -Dversion=5.3 -Dpackaging=jar
+
+echo "Done."
