5db3fd4748456b64dad0058a1464871660d65979
Stephen C Phillips <steve@scphillips.com>
2014-03-10 16:54:35 +0000

    Adds command to remove unpacked ECC webapp to ensure clean re-deployment
    Also adds comments

--- resetECC.sh
+++ resetECC.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
+# stop tomcat
 sudo service tomcat7 stop
+# drop ECC's database and regenerate a clean one
 sudo -u postgres dropdb edm-metrics
 sudo -u postgres createdb -T template0 edm-metrics --encoding=UTF8 --locale=en_US.utf8
 sudo -u postgres psql -d edm-metrics -f edm/resources/edm-metrics-postgres.sql
+# remove the unpacked ECC from tomcat to ensure we get a clean version redeployed
+rm -rf /var/lib/tomcat7/webapps/ECC
+# start tomcat
 sudo service tomcat7 start
