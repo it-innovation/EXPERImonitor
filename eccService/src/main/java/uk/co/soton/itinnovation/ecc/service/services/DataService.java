807801a3b0a66bb280f19d69875a3d8537d7323f
Simon Crowle <sgc@it-innovation.soton.ac.uk>
2014-04-11 13:12:41 +0100

    First implementation of the Data Service

--- eccService/src/main/java/uk/co/soton/itinnovation/ecc/service/services/DataService.java
+++ eccService/src/main/java/uk/co/soton/itinnovation/ecc/service/services/DataService.java
@@ -27,24 +27,44 @@ package uk.co.soton.itinnovation.ecc.service.services;
 
 import java.util.Date;
 import java.util.HashSet;
+import java.util.Map;
+import java.util.Properties;
 import java.util.Set;
 import java.util.UUID;
-import org.openprovenance.prov.xml.Attribute;
-import org.openprovenance.prov.xml.Entity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 import uk.ac.soton.itinnovation.experimedia.arch.ecc.common.dataModel.experiment.Experiment;
+import uk.ac.soton.itinnovation.experimedia.arch.ecc.common.dataModel.metrics.Entity;
+import uk.ac.soton.itinnovation.experimedia.arch.ecc.common.dataModel.metrics.Attribute;
 import uk.ac.soton.itinnovation.experimedia.arch.ecc.common.dataModel.metrics.MeasurementSet;
+import uk.ac.soton.itinnovation.experimedia.arch.ecc.common.dataModel.metrics.MetricGenerator;
+import uk.ac.soton.itinnovation.experimedia.arch.ecc.common.dataModel.metrics.MetricHelper;
+import uk.ac.soton.itinnovation.experimedia.arch.ecc.common.dataModel.metrics.Report;
+import uk.ac.soton.itinnovation.experimedia.arch.ecc.edm.factory.EDMInterfaceFactory;
+import uk.ac.soton.itinnovation.experimedia.arch.ecc.edm.spec.metrics.IMonitoringEDM;
+import uk.ac.soton.itinnovation.experimedia.arch.ecc.edm.spec.metrics.dao.IEntityDAO;
+import uk.ac.soton.itinnovation.experimedia.arch.ecc.edm.spec.metrics.dao.IExperimentDAO;
+import uk.ac.soton.itinnovation.experimedia.arch.ecc.edm.spec.metrics.dao.IMetricGeneratorDAO;
+import uk.ac.soton.itinnovation.experimedia.arch.ecc.edm.spec.metrics.dao.IReportDAO;
 import uk.co.soton.itinnovation.ecc.service.domain.DatabaseConfiguration;
 
 /**
  * Provides access to data in the database.
  */
+@Service("dataService")
 public class DataService {
 
     private final Logger logger = LoggerFactory.getLogger(getClass());
+    
+    private IMonitoringEDM      expDataManager;
+	private IExperimentDAO      experimentDAO;
+	private IMetricGeneratorDAO metricGenDAO;
+    private IEntityDAO          entityDAO;
+    private IReportDAO          expReportDAO;
 
     private boolean started = false;
+    
 
     public DataService() {
     }
@@ -57,8 +77,40 @@ public class DataService {
      * @return
      */
     boolean start(DatabaseConfiguration databaseConfiguration) {
-        started = true;
-        return true;
+        started = false;
+        
+        if ( databaseConfiguration != null ) {
+            
+            // Make sure clear any previously created EDM references
+            expDataManager = null;
+            experimentDAO  = null;
+            metricGenDAO   = null;
+            entityDAO      = null;
+            expReportDAO   = null;
+            
+            // Create properties for EDM Factory
+            Properties props = new Properties();
+            props.put( "dbPassword", databaseConfiguration.getUserPassword() );
+            props.put( "dbName", databaseConfiguration.getDatabaseName() );
+            props.put( "dbType", databaseConfiguration.getDatabaseType() );
+            props.put( "dbURL", databaseConfiguration.getUrl() );
+            props.put( "dbUsername", databaseConfiguration.getUserName() );
+            
+            // Try getting data accessors
+            try {
+                expDataManager = EDMInterfaceFactory.getMonitoringEDM( props );
+                experimentDAO  = expDataManager.getExperimentDAO();
+                metricGenDAO   = expDataManager.getMetricGeneratorDAO();
+                entityDAO      = expDataManager.getEntityDAO();
+                expReportDAO   = expDataManager.getReportDAO();
+                
+                started = true;
+            }
+            catch ( Exception ex ) {
+                logger.error( "Could not start data service: " + ex.getMessage() );
+            }
+        }
+        return started;
     }
 
     public boolean isStarted() {
@@ -74,6 +126,12 @@ public class DataService {
 
         if (!started) {
             logger.error("Failed to shut down data service: not initialised yet");
+            
+            expReportDAO   = null;
+            entityDAO      = null;
+            metricGenDAO   = null;
+            experimentDAO  = null;
+            expDataManager = null;
         }
     }
 
@@ -83,14 +141,24 @@ public class DataService {
      * instances of class Experiment but these will not contain metric models.
      *
      * @return - Returns a set of experiment instances (high-level data only)
-     * @throws Exception - throws if the service is not initialised or there was
-     * a problem with the database
      */
-    Set<Experiment> getAllKnownExperiments() throws Exception {
-
-        HashSet<Experiment> experiments = new HashSet<Experiment>();
-
-        return experiments;
+    Set<Experiment> getAllKnownExperiments() {
+        
+        HashSet<Experiment> result = new HashSet<Experiment>();
+        
+        // Safety first
+        if ( started ) {
+            try {
+                result.addAll( experimentDAO.getExperiments( false ) );
+            }
+            catch ( Exception ex ) {
+                logger.warn( "Could not retrieve experiments: " + ex.getMessage() );
+            }
+        }
+        else
+            logger.warn( "Cannot get experiments: service not started" );
+        
+        return result;
     }
 
     /**
@@ -103,7 +171,22 @@ public class DataService {
      * a problem with the database
      */
     Experiment getExperimentWithMetricModels(UUID expID) throws Exception {
+        
+        // Safety first
+        if ( !started ) throw new Exception( "Cannot get experiment: service not started" );
+        if ( expID == null ) throw new Exception( "Could not get experiment: ID is null" );
+        
         Experiment experiment = null;
+        
+        try {
+            experiment = experimentDAO.getExperiment( expID, true );
+        }
+        catch ( Exception ex ) {
+            String msg = "Could not retrieve experiment: " + ex.getMessage();
+            logger.warn( msg );
+            
+            throw new Exception( msg );
+        }
 
         return experiment;
     }
@@ -114,36 +197,116 @@ public class DataService {
      *
      * @param expID - ID of the experiment you need entities from.
      * @return - Returns a set of entities for the experiment (if any exist)
-     * @throws Exception - throws if the service is not initialised or there was
-     * a problem with the database
+     * @throws Exception - throws if the experiment ID is invalid or there is no experiment that ID
      */
     Set<Entity> getEntitiesForExperiment(UUID expID) throws Exception {
+        
+        // Safety
+        if ( !started ) throw new Exception( "Could not get Entities for experiment: service not started" );
+        if ( expID == null ) throw new Exception( "Could not get Entities for experiment: ID is null" );
+        
         HashSet<Entity> entities = new HashSet<Entity>();
-
+        
+        try {
+            entities.addAll( entityDAO.getEntitiesForExperiment( expID, true ) );
+        }
+        catch ( Exception ex ) {
+            logger.warn( "Could not get entities for experiment ("+ expID.toString() + "): " + ex.getMessage() );
+        }
+        
         return entities;
     }
 
     /**
-     * Use this method to retrieve all measurement sets related to a specific
-     * attribute. In some cases, a client (or clients) may share an attribute
-     * and attach more than one set of measurements for that attribute.
+     * Use this method to retrieve ALL MeasurementSets (WITHOUT measurement data) related to
+     * a specific attribute in an experiment. This method is useful if you want to retrieve all
+     * measurement data (irrespective of which MetricGenerator created it) for a specific attribute.
      *
-     * @param expID - Non-null experiment ID for the experiment the attribute
-     * belongs to
+     * @param expID - Non-null experiment ID for the experiment the attribute belongs to
      * @param attr - Non-null attribute for which measurements are sought
      * @return - Returns (a possibly empty) set of measurement sets
      * @throws Exception - throws if the input parameters are null or invalid
      */
-    Set<MeasurementSet> getMeasurementSetsForAttribute(UUID expID, Attribute attr) throws Exception {
+    Set<MeasurementSet> getAllEmptyMeasurementSetsForAttribute(UUID expID, Attribute attr) throws Exception {
+        
+        // Safety first
+        if ( !started ) throw new Exception( "Cannot get experiments: service not started" );
+        if ( expID == null ) throw new Exception( "Could not get Measurement Sets for Attribute: experiment ID is null" );
+        if ( attr == null ) throw new Exception( "Could not get Measurement Sets for Attribute: Attribute is null" );
+        
         HashSet<MeasurementSet> mSets = new HashSet<MeasurementSet>();
+        
+        // Get metric generators for this experiment
+        try {
+            Set<MetricGenerator> metGens = metricGenDAO.getMetricGeneratorsForExperiment( expID, true );
+            
+            Map<UUID, MeasurementSet> allMemSets = MetricHelper.getMeasurementSetsForAttribute( attr, metGens );
+            mSets.addAll( allMemSets.values() );
+        }
+        catch ( Exception ex ) {
+            String msg = "Could not retrieve all measurement sets for Attribute " + attr.getName() + ": " + ex.getMessage();
+            
+            logger.warn( msg );
+            throw new Exception( msg, ex );
+        }        
 
         return mSets;
     }
+    
+    /**
+     * Use this method to retrieve MeasurementSets (WITHOUT measurement data) for
+     * a specific attribute from a specific MetricGeneator in an experiment.
+     * 
+     * @param mgen        - MetricGenerator that may hold MeasurementSets for an Attribute
+     * @param attr        - Target Attribute which may have measurements associated with it
+     * @return            - returns a set of MeasurementSets for the specific MetricGenerator (if they exist)
+     * @throws Exception  - throws if the parameters are invalid or the MetricGenerator/Attribute could not be found
+     */
+    Set<MeasurementSet> getEmptyMeasurementSetsForAttribute( MetricGenerator mgen, Attribute attr ) throws Exception {
+        
+        // Safety first
+        if ( !started ) throw new Exception( "Cannot get experiments: service not started" );
+        if ( mgen == null ) throw new Exception( "Could not get MeasurementSets for Attribute: MetricGenerator is null" );
+        if ( attr == null ) throw new Exception( "Could not get MeasurementSets for Attribute: Attribute is null" );
+        
+        UUID attrID = attr.getUUID();
+        if ( attrID == null ) throw new Exception( "Could not get MeasurementSets for Attribute: Attribute ID is null" );
+        
+        // Create a result set, then try populating
+        HashSet<MeasurementSet> resultSet = new HashSet<MeasurementSet>();
+        
+        try {
+            Map<UUID, MeasurementSet> entityMSets = MetricHelper.getAllMeasurementSets( mgen );
+            
+            // If there are measurement sets associated with this entity, add those linked to the target entity
+            for ( MeasurementSet ms : entityMSets.values() ) {
+                
+                if ( ms != null ) {
+                    // Check set is mapped to attribute
+                    UUID srcAttrID = ms.getAttributeID();
+                    
+                    // Add it to the result set if good
+                    if ( srcAttrID != null && srcAttrID.equals(attrID) )
+                        resultSet.add( ms );
+                }
+                else
+                    logger.warn( "Found NULL measurement set associated with attribute " + attr.getName() );
+            }
+        }
+        catch ( Exception ex ) {
+            String msg = "Had problems getting MeasurementSet for attribute " + attr.getName() + ": " + ex.getMessage();
+            logger.warn( msg );
+            
+            throw ex;
+        }
+        
+        return resultSet;
+    }
 
     /**
-     * This is an overloaded version of the getMeasurementSetsForAttribute(..)
-     * method in which it is possible to specific a start and end range within
-     * which measurements are sought.
+     * Use this method to retrieve MeasurementSets that are populated with actual 
+     * measurements (where they exist). Using this method requires that you specify 
+     * a start and end range within which measurements are sought.
      *
      * @param expID - Non-null experiment ID for the experiment the attribute
      * belongs to
@@ -156,9 +319,40 @@ public class DataService {
      * @throws Exception - throws if the input parameters are null or invalid
      */
     Set<MeasurementSet> getMeasurementSetsForAttribute(UUID expID, Attribute attr, Date start, Date end) throws Exception {
-        HashSet<MeasurementSet> mSets = new HashSet<MeasurementSet>();
+        
+        // Safety first
+        if ( !started ) throw new Exception( "Cannot get experiments: service not started" );
+        if ( expID == null ) throw new Exception( "Could not get Measurement Sets for Attribute: experiment ID is null" );
+        if ( attr == null ) throw new Exception( "Could not get Measurement Sets for Attribute: Attribute is null" );
+        if ( start == null || end == null ) throw new Exception( "Could not get Measurement Sets for Attribute: date(s) is null" );
+        
+        HashSet<MeasurementSet> resultSet = new HashSet<MeasurementSet>();
+        
+        try {
+            // Get the MeasurementSet model first
+            Set<MeasurementSet> msetInfo = getAllEmptyMeasurementSetsForAttribute( expID, attr );
+            for ( MeasurementSet ms : msetInfo )
+            {
+                // Then populate with data
+                if ( ms != null ) {
+                    
+                    Report report = expReportDAO.getReportForMeasurementsForTimePeriod( expID, start, end, true );
+                    resultSet.add( report.getMeasurementSet() );
+                }
+                else {
+                    String msg = "Had problems retrieving a measurement set: MS ID is null";
+                    logger.warn( msg );
+                    throw new Exception( msg );
+                }
+            }
+        }
+        catch ( Exception ex ) {
+            String msg = "Had problems retrieving measurement set data for Attribute " + attr.getName() + ": " + ex.getMessage();
+            logger.warn( msg );
+            
+            throw new Exception( msg, ex );
+        }
 
-        return mSets;
+        return resultSet;
     }
-
 }
