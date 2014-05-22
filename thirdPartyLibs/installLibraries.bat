c620798d750578678950db36ebff73c1abc219bf
Simon Crowle <sgc@it-innovation.soton.ac.uk>
2014-05-22 09:31:36 +0100

    fixes third party install BAT file

--- thirdPartyLibs/installLibraries.bat
+++ thirdPartyLibs/installLibraries.bat
@@ -1,10 +1,8 @@
+echo off
+
 echo "Installing OWLIM Lite JAR in local Maven repository"
 
-if exist {owlim-lite-5.4.jar} (
-    call mvn install:install-file -Dfile=.\owlim-lite-5.4.jar -DgroupId=com.ontotext -DartifactId=owlim-lite -Dversion=5.4 -Dpackaging=jar
-) else (
-    echo "Could not find owlim-lite-5.4.jar, please place the file in this directory. See README.txt for more information."
-)
+if exist "owlim-lite-5.4.jar" (call mvn install:install-file -Dfile=.\owlim-lite-5.4.jar -DgroupId=com.ontotext -DartifactId=owlim-lite -Dversion=5.4 -Dpackaging=jar) Else (echo "Could not find owlim-lite-5.4.jar, please place the file in this directory. See README.txt for more information.")
 
 rem OpenRDF WAR files are automatically installed using Vagrant; otherwise, please manually deploy these in Tomcat.
 
