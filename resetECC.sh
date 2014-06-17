e8137de0cef00e1c4d77915f8c00debfb9cd3231
Stephen C Phillips <scp@it-innovation.soton.ac.uk>
2014-06-17 16:24:26 +0100

    Clarifies the use of the reset.sh script (issue #10).

--- resetECC.sh
+++ resetECC.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
+# Run this script as root to stop tomcat, clear out PostgreSQL DB and start tomcat.
+# It is mostly useful for developers.
+
 echo "Resetting the ECC"
 
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 
 # stop tomcat
-sudo service tomcat7 stop
+service tomcat7 stop
 # drop ECC's database and regenerate a clean one
 sudo -u postgres dropdb edm-metrics
 sudo -u postgres createdb -T template0 edm-metrics --encoding=UTF8 --locale=en_US.utf8
@@ -15,4 +18,4 @@ sudo -u postgres psql -d edm-metrics -f "$SCRIPTPATH"/edm/resources/edm-metrics-
 rm -rf /var/lib/tomcat7/webapps/ECC
 rm -rf /var/cache/tomcat7/Catalina/localhost/ECC
 # start tomcat
-sudo service tomcat7 start
+service tomcat7 start
