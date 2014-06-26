233389f3526e52a27365e012c489dca6c5900c9f
Stephen C Phillips <scp@it-innovation.soton.ac.uk>
2014-06-26 16:00:41 +0100

    Adds package.sh script to make it trivial to do binary and source releases (helps Jenkins)

---
+++ package.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+cd thirdPartyLibs
+./installLibraries.sh
+mvn package assembly:assembly -Preleasesrc
+mvn package assembly:assembly -Preleasebin
+cd target/experimedia-arch-ecc-2.1-bin/bin
+zip -d EccService-2.1.war WEB-INF/lib/owlim-lite-5.4.jar
+cd ../..
+zip -r experimedia-arch-ecc-2.1-bin.zip experimedia-arch-ecc-2.1-bin
