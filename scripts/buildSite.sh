2affdd75a23b73d1ccec63889d26e9051e3c9305
Stephen C Phillips <scp@it-innovation.soton.ac.uk>
2014-10-20 14:16:15 +0100

    Improves script by running versions plugin and puts output into /tmp

--- scripts/buildSite.sh
+++ scripts/buildSite.sh
@@ -3,4 +3,10 @@ SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"/..
 
 mvn -Pjenkins clean site:site site:stage |tee /tmp/buildSite.log
+mvn versions:display-dependency-updates |tee /tmp/dependency-updates.log
+mvn versions:display-plugin-updates |tee /tmp/plugin-updates.log
+
+echo
 echo "Site now available in target/staging (log file in /tmp/buidSite.log)"
+echo "See /tmp/dependency-updates.log for new dependency versions"
+echo "See /tmp/plugin-updates.log for new plugin versions"
