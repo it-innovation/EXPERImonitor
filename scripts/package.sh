df7036d2401f195d802244c765e0f272a5100ce6
Stephen C Phillips <scp@it-innovation.soton.ac.uk>
2014-11-14 13:13:53 +0000

    Adds more diagnostic info for console.

--- scripts/package.sh
+++ scripts/package.sh
@@ -12,20 +12,26 @@ easy_install -U Sphinx
 apt-get install -y zip
 
 # build the docs
+echo "*** Building docs"
 cd "$ROOT"/doc
 make clean html |tee /tmp/releasedoc.log
 
 # install the third-party libs (later removed from distribution)
+echo "*** Installing third-party libs"
 cd "$ROOT"/thirdPartyLibs
 ./installLibraries.sh
 
 # build
 cd "$ROOT"
+echo "*** maven clean"
 mvn clean
+echo "*** Building source package"
 mvn --batch-mode package assembly:assembly -Preleasesrc |tee /tmp/releasesrc.log |grep -E 'WARNING|ERROR'
+echo "*** Building binary package"
 mvn --batch-mode package assembly:assembly -Preleasebin |tee /tmp/releasebin.log |grep -E 'WARNING|ERROR'
 
 # zip
+echo "Zipping"
 cd "$ROOT"/target/experimedia-arch-ecc-2.2-bin/bin
 zip -d EccService-2.2.war WEB-INF/lib/owlim-lite-5.4.jar
 rm experimedia-arch-ecc-edm-test-2.2.jar  # this jar should not contain anything other than test code, but it does so we delete it here
@@ -34,5 +40,6 @@ zip -r experimedia-arch-ecc-2.2-bin.zip experimedia-arch-ecc-2.2-bin
 
 echo
 echo "Check /tmp/release*.log for further information."
-echo "To update the committed license file data, execute the following command and then commit:"
-echo "cp -a target/experimedia-arch-ecc-2.2-src/licenses ."
+echo "Third-party license information has been aggregated into target/experimedia-arch-ecc-2.2-src/licenses"
+echo "To update the license file data in version control, execute the following command and then commit:"
+echo "cp -a $ROOT/target/experimedia-arch-ecc-2.2-src/licenses $ROOT"
